ab986d43026719b6f875277a3729f50e2514a9f7
Yihang Li <56984624+Yihang-Li@users.noreply.github.com>
2022-05-14 16:20:18 +0800

    update my process

--- C++ Primer Practice/Ch2/p2_1.cpp
+++ C++ Primer Practice/Ch2/p2_1.cpp
@@ -18,5 +18,10 @@ int main()
 
     std::cout << 123456789 << std::endl;
 
+    // int i = 100, sum = 0;
+    // for (int i = 0; i != 10; ++i)
+    //     sum += i;
+    // std::cout << i << " " << sum << std::endl;
+
     return 0;
 }
--- C++ Primer Practice/Ch2/p2_3.cpp
+++ C++ Primer Practice/Ch2/p2_3.cpp
@@ -2,11 +2,22 @@
 
 int main()
 {
-    int i = 0, *p = &i;
-    std::cout << i << ' ' << *p << ' '<< p << std::endl;
-    *p = 7;
-    int j = 9;
+    // int i, &ri = i;
+    // i = 5;
+    // ri = 10;
+    // std::cout << i << " " << ri << std::endl;
+    int i = 1, *p = &i;
+    int j = 5;
+    std::cout << *p << std::endl;
     p = &j;
-    std::cout << i << ' ' << *p << ' ' << p << std::endl;
+    std::cout << *p << std::endl;
+    *p = 10;
+    std::cout << *p << ' ' << j << std::endl;
+    // int i = 0, *p = &i;
+    // std::cout << i << ' ' << *p << ' '<< p << std::endl;
+    // *p = 7;
+    // int j = 9;
+    // p = &j;
+    // std::cout << i << ' ' << *p << ' ' << p << std::endl;
     return 0;
 }
